2d43eaff6bfee70ce4da64b691283921d8872ab2
Anton Luchkin <luchkin1988@gmail.com>
2024-10-19 19:58:18 +0300

    third commit

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -3,12 +3,12 @@ import { Component, Input } from '@angular/core';
 import { ImgUrlPipe } from "../../helpers/pipes/img-url.pipe";
 
 @Component({
-  selector: 'app-profile-card',
-  standalone: true,
-  imports: [ImgUrlPipe],
-  templateUrl: './profile-card.component.html',
-  styleUrl: './profile-card.component.scss'
+	selector: 'app-profile-card',
+	standalone: true,
+	imports: [ImgUrlPipe],
+	templateUrl: './profile-card.component.html',
+	styleUrl: './profile-card.component.scss'
 })
 export class ProfileCardComponent {
-  @Input() profile!: Profile
+	@Input() profile!: Profile
 }
