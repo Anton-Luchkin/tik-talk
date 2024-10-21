b7e84d45156170d381a4cd2437c4d573c4fb0908
Anton Luchkin <luchkin1988@gmail.com>
2024-10-21 12:25:01 +0300

    fourth commit

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,23 +1,17 @@
 import { Component, inject } from '@angular/core';
-import { Profile } from '../../data/interfaces/profile.interface';
 import { ProfileService } from '../../data/services/profile.service';
 import { ProfileCardComponent } from "../../common-ui/profile-card/profile-card.component";
+import { ProfileFiltersComponent } from './profile-filters/profile-filters.component';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [ProfileCardComponent],
+  imports: [ProfileCardComponent, ProfileFiltersComponent],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss'
 })
 export class SearchPageComponent {
   profileService = inject(ProfileService);
-  profiles:Profile[] = [];
-
-  constructor() {
-    this.profileService.getTestAccounts()
-    .subscribe(val => {
-      this.profiles = val;
-    });
-  }
+  profiles = this.profileService.filteredProfiles
 }
+  
